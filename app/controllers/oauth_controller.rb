c98f215bf9080f9eac1076497295e450069e659a
Alexey Poliakov <poliakov.o@gmail.com>
2016-11-29 17:32:11 +0200

    Fix Oauth controller

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -1,22 +1,37 @@
 class OauthController < ApplicationController
+  before_action :check_provider
 
-  def create
-    provider_class = "Oauth::#{params[:provider].try(:capitalize)}".safe_constantize
-    return render json: { errors: ["Invalid oauth provider"] }, status: :unprocessable_entity if !provider_class || params[:provider].blank?
-
-    provider_client = provider_class.new
-    result = provider_client.get_user_data(params[:access_token])
-    return render json: { errors: ["Invalid access_token"] }, status: :unprocessable_entity unless result
-
-    user = User.find_by(provider: params[:provider], uid: result["id"])
-    sign_in(user) && render_user(user) and return if user
-
-    result['uid'] = result.delete('id')
-    user = User.new(result.merge(provider: params[:provider], nickname: params[:nickname]))
-    if user.save
-      sign_in(user) && render_user(user)
+  def login
+    @user = User.find_by(provider: params[:provider], uid: @user_data["id"])
+    if @user
+      sign_in(@user) && render_user(@user)
     else
-      render json: { errors_fields: user.errors, errors: user.errors.full_messages }, status: :unprocessable_entity
+      return(render json: { errors: ['User not registred'] }, status: :unauthorized)
     end
   end
+
+  def sign_up
+    @user_data['uid'] = @user_data.delete('id')
+    @user = User.new(@user_data.merge(provider: params[:provider]).merge(user_params))
+    if @user.save
+      sign_in(@user) && render_user(@user, :created)
+    else
+      render json: { errors_fields: @user.errors, errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:nickname, :email, :name)
+    end
+
+    def check_provider
+      provider_class = "Oauth::#{params[:provider].try(:capitalize)}".safe_constantize
+      return render json: { errors: ["Invalid oauth provider"] }, status: :unprocessable_entity if !provider_class || params[:provider].blank?
+
+      provider_client = provider_class.new
+      @user_data = provider_client.get_user_data(params[:access_token])
+      return render json: { errors: ["Invalid access_token"] }, status: :unprocessable_entity unless @user_data
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   scope '/api' do
     resources :users
     resources :password_resets
+    resources :rss_channels
+    resources :posts
     post   'sign_in',  to: 'sessions#create'
     delete 'sign_out', to: 'sessions#destroy'
-    post   'oauth',    to: 'oauth#create'
+    post   'oauth/sign_in',    to: 'oauth#login'
+    post   'oauth/sign_up',    to: 'oauth#sign_up'
   end
 end
